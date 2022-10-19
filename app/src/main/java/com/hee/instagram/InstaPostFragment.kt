08f2a1040e0c7af75697f242a81ee9a854a83040
pandabaeung <69721328+barti971219@users.noreply.github.com>
2022-10-19 23:31:54 +0900

    인스타 포스트 업로드 화면구현

--- app/src/main/java/com/hee/instagram/InstaPostFragment.kt
+++ app/src/main/java/com/hee/instagram/InstaPostFragment.kt
@@ -1,12 +1,18 @@
 package com.hee.instagram
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 
 class InstaPostFragment : Fragment() {
+    var imageUri : Uri? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -16,4 +22,20 @@ class InstaPostFragment : Fragment() {
         return inflater.inflate(R.layout.insta_post_fragment, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // 변수에 함수를 정의해놓음
+        val imagePickerLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+                val imageUri = it.data!!.data
+                Log.d("instaa", "" + imageUri)
+            }
+
+        imagePickerLauncher.launch(
+            Intent(Intent.ACTION_PICK).apply {
+                this.type = MediaStore.Images.Media.CONTENT_TYPE
+            }
+        )
+    }
 }
